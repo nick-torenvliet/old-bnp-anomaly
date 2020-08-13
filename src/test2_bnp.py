61c3d83d21459a77b427bf0d3dab1df55155f58e
Nick Torenvliet <n_torenvliet@hotmail.com>
2020-08-13 19:04:57 -0400

    test output for ds01 with first 10000 data points

--- src/test2_bnp.py
+++ src/test2_bnp.py
@@ -5,6 +5,7 @@ import pandas as pd
 import numpy as np
 import os
 import time
+from glob import glob
 
 batch_size = 5
 window_size_in_batches = 5
@@ -15,36 +16,45 @@ def run_bnp_anomaly(mppack):
     batch_size = mppack[1]
     window_size_in_batches = mppack[2]
     data_set = mppack[3]
+    G = mppack[4]
+
     wds = len(windows)
     gamma = 1.0
     sF = 1.0
     K = 25  # Initialize K component - this value places a max K the model can develop
     nLap = 20
+    ds = data_set[0] + "." + data_set[1]
+
     iname='randexamples'
-    opath = f'/tmp/bnp-anomaly/coldstart/2{data_set}/b0'  # Dynamic output path according to batch
+    opath = f'/tmp/bnp-anomaly/coldstart/{ds}/b0'  # Dynamic output path according to batch
     ll = [np.nan] * window_size_in_batches
     
-    data_df = pd.DataFrame(columns =['index', 'data'])
-    results_df = pd.DataFrame()
+    data_df = pd.DataFrame()
+    calc_df = pd.DataFrame()
+    
     
     for ii, window in enumerate(windows):
+
+        df_index = window[0]
+        xdata_data = window[1]
+
         if ii % 5 == 0:
             print("XXX" + str(data_set)+ " " + str(ii)+"/"+str(wds))
 
         warm_start_model, warm_info_dict = bnpy.run(
-            window, 'DPMixtureModel', 'DiagGauss', 'memoVB',
+            xdata_data, 'DPMixtureModel', 'DiagGauss', 'memoVB',
             output_path=opath,
             nLap=nLap, nTask=1, nBatch=window_size_in_batches, convergeThr=0.0001,
             gamma0=gamma, sF=sF, ECovMat='eye',
             K=K, 
             moves='birth,merge,delete,shuffle',
             initname=iname,
-            ts=True, debug=False, verbose=0, G=2)
+            ts=True, debug=False, verbose=0, G=G)
         
         iname=warm_info_dict['task_output_path']
-        opath = f'/tmp/bnp-anomaly/warmstart/2{data_set}/b{ii +  1}'
+        opath = f'/tmp/bnp-anomaly/warmstart/{ds}/b{ii +  1}'
 
-        batch = window.make_subset(list(range(batch_size * window_size_in_batches - batch_size, batch_size * window_size_in_batches)))
+        batch = xdata_data.make_subset(list(range(batch_size * window_size_in_batches - batch_size, batch_size * window_size_in_batches)))
 
         LP = warm_start_model.calc_local_params(batch)
         SS = warm_start_model.get_global_suff_stats(batch, LP)
@@ -55,61 +65,44 @@ def run_bnp_anomaly(mppack):
         ll_normed = [i/sum(ll) for i in ll]
         entropy = -sum([i*np.log(i) for i in ll_normed])
 
-        K_resp = np.mean(LP["resp"], axis=0)
-        x_window  = SS.x
-        xx_window = SS.xx
+        approx_ll = warm_start_model.calc_evidence(batch)
 
-        x_window = np.vstack(x_window)
-        xx_window = np.vstack(xx_window)
+        index = np.array(df_index.iloc[-1], dtype=int)
+        x = df_index[len(df_index) - batch_size:]
+        xx = df_index[-1:]
+        data_df = data_df.append(pd.DataFrame({'x':x, 'y':batch.X.reshape((batch_size,))}), ignore_index=True)
+        calc_df = calc_df.append(pd.DataFrame({'x':xx, 'LL':LL, 'entropy':entropy, 'approx_LL':approx_ll}), ignore_index=True)
 
-        index = int(ii * batch_size) + window_size_in_batches * batch_size - 1
-        x_window = x_window.flatten()
-        x_window = x_window[x_window >1]
-        xx_window = xx_window.flatten()
-        y = (window.X[-batch_size:])[0::1]
-        y = y.flatten()
-        x = list(range(ii * batch_size, ii * batch_size + window_size_in_batches * batch_size))[-batch_size:][0::1]
-        ddf = pd.DataFrame(columns =['index', 'data'])
-        ddf['data'] = np.array(y)
-        ddf['index'] = np.array(x)
-        data_df = data_df.append(ddf, ignore_index=True)
-        results_df = results_df.append({'index':index, 'LL':LL, 'entropy':entropy}, ignore_index=True)
-        print('holdup')
-    results_df.set_index('index', inplace=True)
-    data_df.set_index('index', inplace=True)
-    return [data_df, results_df, data_set]
+    ds = data_set[0] + "." + data_set[1]
+    name = "data/test_output/" + ds + "_alg-" + str(G) +  "_bs-" + str(batch_size) \
+                + "_wsib-" + str(window_size_in_batches) + ".csv"
+    data_df.set_index('x', inplace=True)
+    calc_df.set_index('x', inplace=True)
+    pd.concat([data_df, calc_df],axis=1, sort=False).to_csv(name)
+    return 0
 
-data_sets = ["./data/test/ds0.csv",
-             "./data/test/ds1.csv",
-             "./data/test/ds2.csv",
-             "./data/test/ds3.csv",
-             "./data/test/ds4.csv",
-             "./data/test/ds5.csv",
-             "./data/test/ds6.csv",
-             "./data/test/ds7.csv"
-            ]
-data = [pd.read_csv(i, usecols=['0']) for i in data_sets]
+G=2
+test_data_dir = "data/test/"
+test_data_files = sorted(glob(test_data_dir + '/ds01*.*.csv'))
+test_data_names = [i.split("/")[2].split(".")[0:2] for i in test_data_files]
+data = [pd.read_csv(i) for i in test_data_files]
 
 for d, df in enumerate(data):
     win = []
     i = 0
-    while i * batch_size < (len(df) - window_size_in_batches * batch_size):
-        w = df[i * batch_size:i * batch_size + window_size_in_batches * batch_size]
-        win.append(bnpy.data.XData.from_dataframe(w))
+    while i * batch_size <= (len(df) - window_size_in_batches * batch_size):
+        w= df[i * batch_size:i * batch_size + window_size_in_batches * batch_size]
+        y = w.drop(columns=['Unnamed: 0'])
+        yy = bnpy.data.XData.from_dataframe(y)
+        x = w['Unnamed: 0']
+        win.append([x, yy])
         i += 1
-    windows.append([win, batch_size, window_size_in_batches, d])
+    windows.append([win, batch_size, window_size_in_batches, test_data_names[d], G])
 
-alg = "G2"
+
 pool =  mp.Pool(mp.cpu_count())
 results = pool.map(run_bnp_anomaly, windows)
 pool.close()
-for results in results:
-    data = results[0]
-    calc = results[1]
-    dset = results[2]
-    data.to_csv("data/test_output/dataset" + str(dset) + "_data_test_alg" + alg +  "_bs" + str(batch_size) 
-                + "_wsib" + str(window_size_in_batches) + ".csv")
-    calc.to_csv("data/test_output/dataset" + str(dset) + "_results_test_alg" + alg +  "_bs" + str(batch_size) 
-                + "_wsib" + str(window_size_in_batches) + ".csv")
 
-    
+for w in windows:
+    run_bnp_anomaly(w)
